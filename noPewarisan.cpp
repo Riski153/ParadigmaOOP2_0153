f63b9a691390aabfb47de7485ef10ba632f48ffe
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-05-19 10:53:46 +0700

    menambahkan class baseClass final

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class baseClass final {};
+class baseClass final {
+    public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
